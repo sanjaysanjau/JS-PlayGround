0d8dd37fdfbaaf470dbc7cecc7837da212dbbaec
sanjaysanjau <sanjaysmart979@gmail.com>
2024-09-24 12:29:59 +0530

    sample

---
+++ test.js
@@ -0,0 +1,23 @@
+var name="sanjay"
+
+const getName = () =>{
+    let name="usha";
+console.log("name=",name)
+return name;
+}
+console.log(getName())
+
+let a=[1,2,3]
+let b=[...a]
+b.push(4)
+console.log(a);
+
+const value='345'
+console.log(Boolean(value))
+
+let x=0
+// console.log(x++)
+// console.log(++x)
+
+while(x++ <5,x<3)
+    console.log(x)
